d0e5c5d4b3d4b8c04fee2c5a2193fde25a723fcc
Abdul Aleem <ma.aleem20@gmail.com>
2022-12-24 22:36:24 +0400

    mysql.commint

--- roles/mysql/templates/app-user.sql
+++ roles/mysql/templates/app-user.sql
@@ -1,4 +1,4 @@
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '{{ROOT_PASSWORD}}';
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '{{ROOT_PASSWORD}}';
 
 create database if not exists cities;
 GRANT ALL ON cities.* TO 'shipping'@'%' IDENTIFIED BY 'secret';
